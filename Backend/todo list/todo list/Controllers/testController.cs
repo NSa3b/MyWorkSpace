1f1b790bfe2601846c5a1197877d5014eba9d1c6
Noha Saab <noha.sa3b@gmail.com>
2022-11-09 16:23:32 +0200

    first commit

---
+++ Backend/todo list/todo list/Controllers/testController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace todo_list.Controllers
+{
+    [Route("api/[controller]")]  //api/test(tooken replacemnet) ___________ {} parameter passed in curly braces
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public string getall()
+        {
+            return "welcome nahnoha";
+        }
+
+    }
+}
---
+++ Frontend/index.js
@@ -0,0 +1,87 @@
+let addBtn=document.getElementById("addbtn");
+let newtext=document.getElementById("newTask");
+let listTitle=document.getElementById("listTitle");
+let listItems=document.querySelectorAll(".listItem button");
+
+function addTask() {
+
+    if(newtext!=""){
+
+        let newli =document.createElement("li");
+        let newContainer=document.createElement("div")
+        let newTask=document.createElement("p");
+        let newCheckBtn=document.createElement("button");
+        let newDeleteBtn=document.createElement("button");
+    
+        newTask.textContent=newtext.value;
+        newtext.value="";
+
+
+        for(let item of listItems)
+        {
+            if(item.className.includes("chosenList")){
+                let color=item.className.slice(0,item.className.indexOf(" "));
+                newCheckBtn.classList.add(color+"btn","checkbtn");
+               
+            };
+        }
+
+        newDeleteBtn.classList.add("btn","btn-close","me-2");
+
+
+
+        newCheckBtn.onclick= function(){
+            if(newli.className!="checked"){
+
+                newli.classList.add("checked");
+            }
+            else{
+                newli.classList.remove("checked");
+            }
+        }
+
+        newDeleteBtn.onclick=function(){
+            newli.remove();
+        }
+
+
+        newContainer.append(newCheckBtn,newTask);
+        newli.append(newContainer,newDeleteBtn);
+    
+        let todoList=document.getElementById("todoList");
+        todoList.appendChild(newli);
+
+        
+    };
+    
+};
+
+
+function init() {
+    addBtn.addEventListener("click", addTask);
+
+    for(let item of listItems){
+
+        item.onclick=function(){
+    
+            for(let i of listItems)
+            {
+                i.classList.remove("chosenList");
+            }
+    
+            item.classList.add("chosenList");
+            listTitle.innerHTML=item.innerHTML;
+        };
+        
+    };
+}
+
+init();
+
+
+
+ 
+    
+
+
+
